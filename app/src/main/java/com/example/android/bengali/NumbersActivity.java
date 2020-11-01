2fc91283bdeb213da9ee7908008e050cb2a5032c
eshawn <eshawn.karim@gmail.com>
2020-11-01 18:59:20 -0500

    Add ColorsActivity.java, FamilyActivity.java, PhrasesActivity.java & NumbersActivity.java
    Signed-off-by: eshawn <eshawn.karim@gmail.com>

---
+++ app/src/main/java/com/example/android/bengali/NumbersActivity.java
@@ -0,0 +1,27 @@
+/*
+ * *
+ *  * Created by Eshawn Karim on 11/1/20 6:59 PM
+ *  * Last modified 11/1/20 6:58 PM
+ *  *
+ *  * NumbersActivity.java
+ *  * Bengali
+ *  *
+ *  * www.EshawnKarim.us
+ *  * Copyright (c) 2020.
+ *
+ */
+
+package com.example.android.bengali;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+    }
+}
