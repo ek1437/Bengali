4dd18d1eb2f576068b67c4953511119f27c9afb0
eshawn <eshawn.karim@gmail.com>
2020-11-01 17:29:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/bengali/MainActivity.java
@@ -0,0 +1,31 @@
+
+/*
+ * *
+ *  * Created by Eshawn Karim on 11/1/20 5:26 PM
+ *  * Last modified 11/1/20 5:05 PM
+ *  *
+ *  * MainActivity.java
+ *  * Bengali
+ *  *
+ *  * www.EshawnKarim.us
+ *  * Copyright (c) 2020.
+ *
+ */
+
+package com.example.android.bengali;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+    }
+}
